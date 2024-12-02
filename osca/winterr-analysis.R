d202e7f2ea1c681e2bc8db7788ca05cc2788cfaf
=jwhalfgit <=john.halfacre@york.ac.uk>
2024-12-02 12:25:02 +0000

    unknown

--- osca/winterr-analysis.R
+++ osca/winterr-analysis.R
@@ -2,7 +2,8 @@
 # Load files ----------------------------------------------------------------
 
 library(openair)
-
+library(ncdf4)
+library(lubridate)
 OSCADATADIR <- file.path(EXPTDIR, "OSCA","data")
 
 tildas <- read_csv(file.path(OSCADATADIR,"2022", "tildas.csv")) %>% 
@@ -13,13 +14,50 @@ clno2 <- read_csv(file.path(OSCADATADIR,"2022", "cims60sAvg.csv")) %>%
   rename(date = ts)%>% 
   select(!contains(c("N2O5", "Cl2")))
 
-
-specrad <-read_csv(file.path(OSCADATADIR,"2022", "specrad.csv")) %>% 
-  rename(date = datetime)
-
 n2o5_cl2 <- read_csv(file.path(OSCADATADIR,"2022", "cims-n2o5-cl2.csv")) %>% 
   rename(date = ts) %>% 
-  select(!contains("N2O5"))
+  select(!contains("HCN"))
+
+
+no2 <- read_csv(file.path(OSCADATADIR,"2022","no2.csv")) %>% 
+  rename(date = datetime, no2 = `NO2 (ppb)`) %>% 
+  select(!`NO2_qc_flags`)
+
+nonoy <- read_csv(file.path(OSCADATADIR,"2022","nox-noy.csv")) %>% 
+  rename(date = datetime, no=`NO (ppb)`, noy = `NOy (ppb)`)
+
+o3 <- read_csv(file.path(OSCADATADIR,"2022", "o3.csv")) %>% 
+  rename(date = datetime, o3 = "Ozone (ppb)") %>% 
+  select(!contains("qc"))
+
+
+so2 <- read_csv(file.path(OSCADATADIR,"2022", "noy_so2_yrk.csv")) %>% 
+  select(date, contains("so2")) %>% 
+  filter(qc_flag_so2_43i %in% c(0,1,2)) %>% 
+  rename(so2 = so2_43i) %>% 
+  select(date,so2)
+
+
+
+voc <- read_csv(file.path(OSCADATADIR,"2022", "gc.csv")) %>% 
+  mutate(date = dmy_hm(date)) %>% 
+  select(!contains("flag")) %>% 
+  select(!contains("_w")) %>% 
+  filter(type == "Sample") %>% 
+  select(!contains("uncertainty")) %>% 
+  select(-c("...1"))
+
+
+
+# This is the estimated BL height in m
+pblh <- read_csv(file.path(OSCADATADIR,"2022","maqs.csv")) %>% 
+  select(ts, "Estimated Boundary Layer Height (m)") %>% 
+  rename(date = ts, pblh = "Estimated Boundary Layer Height (m)")
+
+specrad <-read_csv(file.path(OSCADATADIR,"2022", "specrad.csv")) %>% 
+  rename(date = datetime, jo1d = "JO1D (s-1)", jno2 = "JNO2 (s-1)")
+
+
 
 acsm <- read_csv(file.path(OSCADATADIR,"2022", "acsm.csv")) %>% 
   mutate(datetime = dmy_hm(datetime)) %>% 
@@ -43,11 +81,79 @@ xact10 <- read_csv(file.path(OSCADATADIR,"2022", "xact10.csv")) %>%
   rename(date = datetime, pcl_10 = "PM10_Cl 17 (ug/m3)",
          pcl_10_unc = "PM10_Cl Uncert (ug/m3)")
 
+smps <- read_csv(file.path(OSCADATADIR,"2022", "smps.csv")) %>% 
+  mutate(datetime = dmy_hm(datetime)) %>% 
+  rename(date = datetime)
+
+
+
+
+voc <- read_csv(file.path(OSCADATADIR,"2022", "gc.csv")) %>% 
+  mutate(date = dmy_hms(date)) %>% 
+  select(!contains("flag")) %>% 
+  select(!contains("_w")) %>% 
+  filter(type == "Sample") %>% 
+  select(!contains("uncertainty")) %>% 
+  select(-c("...1"))
+
 
 met <- read_csv(file.path(OSCADATADIR,"2022", "fidas-met.csv")) %>% 
   mutate(datetime = dmy_hm(datetime)) %>% 
   select(!contains("flag"))
 names(met) = c("date","tempC","press","rh","tempK")
+
+
+
+dfMaster <- tildas %>% 
+  full_join(clno2) %>% 
+  full_join(no2) %>% 
+  full_join(nonoy) %>% 
+  full_join(n2o5_cl2) %>% 
+  full_join(o3) %>% 
+  full_join(so2) %>% 
+  full_join(voc) %>% 
+  full_join(pblh) %>% 
+  full_join(met) %>% 
+  full_join(acsm) %>% 
+  full_join(xact2_5) %>% 
+  full_join(xact10) %>% 
+  full_join(specrad) %>% 
+  full_join(smps) %>% 
+  arrange(date) %>% 
+  select(!contains("Diff")) %>% 
+  select(!contains("qc"))
+
+dfMasterOut <- dfMaster %>%
+  filter(between(date, ymd("2022-02-01"), ymd("2022-02-28"))) %>% 
+  mutate(date = floor_date(date,"10 min")) %>% 
+  group_by(date) %>% 
+  summarise_all(mean,na.rm=TRUE)
+
+
+
+write_csv(dfMasterOut, file.path(OSCADATADIR, "2022", "dfMaster-10min.csv"))
+
+
+
+
+
+
+
+
+
+# Specrad -----------------------------------------------------------------
+
+specradFeb <- nc_open(file.path(OSCADATADIR,"2022","specrad-feb.nc"))
+ncVar <- names(specradFeb$var)
+
+for(i in 1:length(ncVar)) {
+  assign(ncVar[i], ncvar_get(specradFeb, ncVar[i]))
+}
+
+sTime <- as.POSIXct(paste(year, day_of_year, hour, minute, second, sep = " "),
+                   format = "%Y %j %H %M %S",tz = "UTC")
+
+
 
 # met %>%
 #   filter(between(hour(date), 8, 20))
