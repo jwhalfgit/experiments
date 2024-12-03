d52ee1af4e82a01d93c562f9c08a5592a729b9a1
=jwhalfgit <=john.halfacre@york.ac.uk>
2024-12-03 12:34:55 +0000

    enabled ability to add multiple sites to CONTROL files

--- hysplit/make.conc.control.R
+++ hysplit/make.conc.control.R
@@ -13,16 +13,26 @@ library(xts)
 # Run lengths
 # Met files
 
-met.file.loc <- "" # HYSPLIT met file directory
-wd.hysplit <- "/home/halfacre/hysplit/trunk/working" # HYSPLIT working directory
+met.file.loc <- "C:/HYSPLIT/metfiles/" # HYSPLIT met file directory
+wd.hysplit <- "C:/HYSPLIT/trunk/working" # HYSPLIT working directory
+
+sites <- as.data.frame(matrix(data = c(53.4542503,-2.2487979,"Manchester Air Quality Site",
+                                       52.476145, -1.874978, "Birmingham Roadside Site"),ncol=3,byrow=TRUE,dimnames = list(c(),c("lat","lon","loc"))))
+
+sites$lat <- as.numeric(sites$lat)
+sites$lon <- as.numeric(sites$lon)
+
+# GDAS Data Codes ---------------------------------------------------------
+
 
 # get.met.code will take a time and return
 # a code that is used to identify the met files
-# needed for the analysis.
+# needed for the analysis.  NOTE THIS IS ONLY 
+# NEEDED FOR GDAS DATA!! NOT USEFUL FOR REANALYSIS DATA
 get.met.code <- function(TIME){
   month <- format(TIME, "%m")
   day <- as.numeric(format(TIME,"%d"))
-  
+
   if(day%in%1:7){
     week <- "w1"
   }else if(day%in%8:14){
@@ -37,63 +47,91 @@ get.met.code <- function(TIME){
   return(paste(month,week,sep="."))
 }
 
+
+
+
 # met.files.ctrl() will take two outputs of 
 # get.met.code(), a start code and an end code,
 # and return the corresponding met file names.
 # NOTE: THIS WILL NOT WORK FOR DATA AFTER JULY!!!
 # (technically won't work for any data without the
 # corresponding met files)
-met.files.ctrl <- function(START.CODE, END.CODE, MET.FILE.LIST){
-  month.st <- as.numeric(strsplit(START.CODE,"[.]")[[1]][1])
-  month.et <- as.numeric(strsplit(END.CODE,"[.]")[[1]][1])
-#   if(month.st>=7||month.st<=2||month.et>=7||month.et<=2){
-#     return(NULL)
-#   }else{
-    start.ix <- grep(START.CODE, MET.FILE.LIST)
-    end.ix <- grep(END.CODE, MET.FILE.LIST)
-    return(MET.FILE.LIST[(end.ix-1):(start.ix+1)])
+# met.files.ctrl <- function(START.CODE, END.CODE, MET.FILE.LIST){
+#   month.st <- as.numeric(strsplit(START.CODE,"[.]")[[1]][1])
+#   month.et <- as.numeric(strsplit(END.CODE,"[.]")[[1]][1])
+# #   if(month.st>=7||month.st<=2||month.et>=7||month.et<=2){
+# #     return(NULL)
+# #   }else{
+#     start.ix <- grep(START.CODE, MET.FILE.LIST)
+#     end.ix <- grep(END.CODE, MET.FILE.LIST)
+#     return(MET.FILE.LIST[(end.ix-1):(start.ix+1)])
+#   #}
+# }
+
+
+# This is for the renalysis(RA) met data
+met.files.ctrl.RA <- function(START, END, MET.FILE.LIST){
+  month.st <- format(START, "%Y%m")
+  month.et <- format(END, "%Y%m")
+
+  start.ix <- grep(pattern = month.st, x=  MET.FILE.LIST)
+  end.ix <- grep(pattern = month.et, x= MET.FILE.LIST)
+  return(MET.FILE.LIST[(end.ix-1):(start.ix+1)])
   #}
 }
 
-met.file.list.syntax <- function(MET.FILES,YEAR){
+#GDAS versuib
+# met.file.list.syntax <- function(MET.FILES,YEAR){
+#   MET.FILES.VEC <- list()
+#   for(ix in 1:length(MET.FILES)){
+#     MET.FILES.VEC[[ix]] <- c(paste(met.file.loc,YEAR,"/",sep=""),MET.FILES[ix])
+#   }
+#   return(unlist(MET.FILES.VEC))
+# }
+
+
+met.file.list.syntax <- function(MET.FILES){
   MET.FILES.VEC <- list()
   for(ix in 1:length(MET.FILES)){
-    MET.FILES.VEC[[ix]] <- c(paste(met.file.loc,YEAR,"/",sep=""),MET.FILES[ix])
+    MET.FILES.VEC[[ix]] <- c(met.file.loc,MET.FILES[ix])
   }
   return(unlist(MET.FILES.VEC))
 }
 
+##################################################################
+
+
+
 # time.loc() will build a data.frame of times and GPS locations
 # to be injected into separate CONTROL files.  It will determine
 # the number of runs for each event.
-time.loc <- function(START,END, GPS){
+time.loc <- function(START,END){
   START <- as.POSIXct(START, tz='UTC')
   END <- as.POSIXct(END,tz='UTC')
   
-  # Step 1: Generate a list of trajectory start times.  
+  # Step 1: Generate a vector of trajectory start times.  
   # This will also tell us how many trajectories we will run.
-  # I think it makes sense to run one each hour during the ODE
+  # I think it makes sense to run one each hour during an event
   # since we have batch capabilities.
   time.seq <- seq.POSIXt(from=START,to=END,by="hours")
   
   # Step 2: Convert each time into a start time usable by 
   # HYSPLIT.  This is "YY MM DD HH MM SS" (but we won't use MM SS).
   st.hysplit <- sapply(time.seq, function(x){format(x,"%y %m %d %H")})
-  if(length(GPS)==2){
-    gps.hysplit <- data.frame(lat=GPS[2],lon=GPS[1])
-  }else{
-    gps.hysplit <- GPS[sapply(time.seq,function(x){which.min(abs(GPS$ts-x))}),2:3]
-  }
+  # if(length(GPS)==2){
+  #   gps.hysplit <- data.frame(lat=GPS[1],lon=GPS[2])
+  # }else{
+  #   gps.hysplit <- GPS[sapply(time.seq,function(x){which.min(abs(GPS$ts-x))}),2:3]
+  # }
   
   # Step 3: Now combine into a data frame so they can come out at once. 
-  time.gps.df <- data.frame(st.hysplit,gps.hysplit,row.names=NULL)
-  names(time.gps.df) <- c("start.time","lat","lon")
-  time.gps.df$start.time <- as.character(time.gps.df$start.time)
-  # Step 4: Round GPS value to the nearest 0.25, consistent with the resolution of the HYSPLIT
-  time.gps.df$lat <- round(time.gps.df$lat * 4, 0)/4
-  time.gps.df$lon <- round(time.gps.df$lon * 4, 0)/4
+  time.df <- data.frame(st.hysplit,row.names=NULL)
+  names(time.df) <- c("start.time")
+  time.df$start.time <- as.character(time.df$start.time)
+  # Step 4: Round GPS value to the nearest degree.  Resolution of Met files is 2.5deg.
   
-  return(time.gps.df)
+  
+  return(time.df)
 }
 
 
@@ -101,111 +139,117 @@ time.loc <- function(START,END, GPS){
 # user-defined variables.  HEIGHTS represents height of particle release, RT is run time,
 # VERT represents the vertical transport method.  Met files typially have this 
 # within them, so I just leave it at 0.  
-build.ctrl <- function(TIME.LOC, HEIGHTS=10, RT=-24, VERT=0, BL.height = 4000, WD){
-
+build.ctrl <- function(TIME.LOC, GPS, HEIGHTS=10, RT=-24, VERT=0, BL.height = 4000, 
+                       WD = wd.hysplit){
+  gps <- GPS[1:2]
+  gps$lat <- round(as.numeric(gps$lat), 0)
+  gps$lon <- round(as.numeric(gps$lon), 0)
+  
+  
   no.heights = length(HEIGHTS)
   # Generate the list of locations/heights
-  loc <- sapply(HEIGHTS,function(x){paste(TIME.LOC[2], TIME.LOC[3], x, sep=" ")})
   
-  traj.st <- as.POSIXct(TIME.LOC[1], format="%y %m %d %H")
-  name <- format(traj.st,"%Y%m%d%H")
+  locList <- list()
+  for(ix in 1:nrow(gps)){
+    for(iy in 1:no.heights){
+      locList[ix] <- paste(gps[ix,1], gps[ix,2], HEIGHTS[iy], sep=" ")
+    }
+  }
+  loc <- unlist(locList)
+  
+  #loc <- sapply(HEIGHTS,function(x){paste(GPS[1], GPS[2], x, sep=" ")})
+  
+  
+  traj.st <- as.POSIXct(TIME.LOC[1], format="%y %m %d %H",tz = "UTC")
+  NAME <- format(traj.st,"%Y%m%d%H")
     
   # Need to determine which met files we need.
   traj.et <- traj.st - as.difftime(-RT,units="hours")
-  met.file.list <- list.files(paste(met.file.loc,format(traj.st,"%Y"),sep=""))
-  met.files <- met.files.ctrl(START.CODE=get.met.code(traj.st),END.CODE=get.met.code(traj.et),
+  # met.file.list <- list.files(paste(met.file.loc,format(traj.st,"%Y"),sep=""))
+  met.file.list <- list.files(met.file.loc)
+  met.files <- met.files.ctrl.RA(START=traj.st,END=traj.et,
                               MET.FILE.LIST=met.file.list)
   if(is.null(met.files)){
     return(NULL)
   }
   # Afterward, rearrange it into the proper CONTROL file syntax
-  met.syntax <- met.file.list.syntax(met.files,YEAR=format(traj.st,"%Y"))
+  met.syntax <- met.file.list.syntax(met.files)
   
-  fileCon <- file(paste("CONTROL",name,sep="."))
+  fileCon <- file(paste("CONTROL",NAME,sep="."))
   #SETUP INFO
   # includes lines for start time, location, heights, met files../.
-  writeLines(c(TIME.LOC[1], # Traj start time
+  writeLines(as.character(c(TIME.LOC[1], # Traj start time
                no.heights, # number of starting heights
                loc, # lat,lon,starting height
                RT, # run time
                VERT, # vertical motion method
                BL.height, # top of the model
                length(met.files), # number of met files
-               met.syntax, #filepaths for relevant met files,
-               "1", # pollutant no.
-               "O3", #name of said pollutant
-               "1.0", # emission rate (units/hr)
-               "1.0", # hours of emission
-               "00 00 00 00 00", # release start time, all 0's default to model start
-               "1",  # grid no.
-               "0.0 0.0", # center of lat /lon
-               "0.25 0.25", # grid spacing
-               "30.0 180", # grid span from starting loc
-               paste(WD,"/",sep=""), # output dir
-               paste("cback-",name,".bin",sep=""), # conc output filename
-               "1", # no of vertical levels
-               "50", #averaging height, i.e. 0-Xm where x is the top of the avg height
-               "00 00 00 00 00", #sampling start time
-               "00 00 00 00 00", # sampling stop time
-               paste("00 ",abs(RT) ," 00",sep=""), # Avg vs inst, hours across whicht o avg 
-               "1", #dep
-               "0.0 0.0 0.0", #dep
-               "0.0 0.0 0.0 0.0 0.0", #dep
-               "0.0 0.0 0.0", #dep
-               "0.0", #dep
-               "0.0"),#dep
+               met.syntax)), #filepaths for relevant met files,
+               # "1", # pollutant no.
+               # "O3", #name of said pollutant
+               # "1.0", # emission rate (units/hr)
+               # "1.0", # hours of emission
+               # "00 00 00 00 00", # release start time, all 0's default to model start
+               # "1",  # grid no.
+               # "0.0 0.0", # center of lat /lon
+               # "0.25 0.25", # grid spacing
+               # "30.0 180", # grid span from starting loc
+               # paste(WD,"/",sep=""), # output dir
+               # paste("cback-",NAME,".bin",sep=""), # conc output filename
+               # "1", # no of vertical levels
+               # "50", #averaging height, i.e. 0-Xm where x is the top of the avg height
+               # "00 00 00 00 00", #sampling start time
+               # "00 00 00 00 00", # sampling stop time
+               # paste("00 ",abs(RT) ," 00",sep=""), # Avg vs inst, hours across whicht o avg 
+               # "1", #dep
+               # "0.0 0.0 0.0", #dep
+               # "0.0 0.0 0.0 0.0 0.0", #dep
+               # "0.0 0.0 0.0", #dep
+               # "0.0", #dep
+               # "0.0"),#dep
                 fileCon)
   
   close(fileCon)
 }
 
 
-buoy14.alltimes <- seq(from=as.POSIXct("2016-04-20 00:00:00",tz="UTC"), 
-                      to = as.POSIXct("2016-05-22 00:00:00",tz="UTC"), by="hour")
+projTimes <- seq(from=as.POSIXct("2024-04-20 01:00:00",tz="UTC"), 
+                      to = as.POSIXct("2024-04-22 01:00:00",tz="UTC"), by="hour")
 
-make.ctrl.files <- function(LOC, YEAR, OB.TIMES = buoy14.alltimes){
-  ode.time.file <- list.files(wd.ode,full.names=TRUE)[grep(paste(LOC,YEAR,sep="-"),list.files(wd.ode))]
-  # the above line returns the file that matches the pattern "LOC-YEAR" in the
-  # ODE times working directory.
-  
+make.ctrl.files <- function(LOC = sites, TIMES = projTimes){
+
   # Now we should load the file and create vectors of the start and stop times:
-  # ode.times <- read.csv(ode.time.file,stringsAsFactors=FALSE,header=TRUE)
-  st <- OB.TIMES[1]
-  et <- OB.TIMES[length(OB.TIMES)]
-  # good job.  Now GPS which calls on the load.data function in load.files.R:
-  gps <- load.data(loc=LOC, year=YEAR,what="gps")
-  if(is.xts(gps)==TRUE){
-    gps <- data.frame(ts=index(gps),coredata(gps[1:2]))
-  }
+  st <- TIMES[1]
+  et <- TIMES[length(TIMES)]
+
+  gps <- LOC[,1:2]
+
   # time.loc() will build a data frame for each event with GPS and times throughout
   # the trajectory.  I guess I need to mapply it?
-  ctrl.files <- mapply(time.loc,OB.TIMES[1],OB.TIMES[length(OB.TIMES)],
-                       MoreArgs=list(GPS=gps),SIMPLIFY=FALSE)
+  ctrl.files <- time.loc(START = st, END = et)
   # Make appropriate directory tree:
   # buoyN
   # bin control particle plot
   # and then place control files within the "control" directory
   
-  for(ix in 1:length(ctrl.files)){
-    ed <- file.path(wd.hysplit,LOC,YEAR,ix) # ed for "event directory"
+  #for(ix in 1:length(ctrl.files)){
+
+    # ed <- file.path(wd.hysplit,LOC,YEAR,ix) # ed for "event directory"
+    ed <- file.path(wd.hysplit) # ed for "event directory"
     cd <- file.path(ed,"control")
     bd <- file.path(ed, "bin")
     pd <- file.path(ed, "particle")
     plot.d <- file.path(ed, "plot")
     sapply(c(ed,cd,bd,pd, plot.d),dir.create,showWarnings=FALSE)
     setwd(cd)
-    ctrl.list <- apply(ctrl.files[[ix]],1,build.ctrl,WD=bd)
+    ctrl.list <- apply(ctrl.files,1,build.ctrl,GPS = gps, WD=bd)
     setwd(wd.hysplit)
-  }
+  #}
 
   return(ctrl.files)
 }
 
-
-
-
-buoy1.alltimes <- seq(from=as.POSIXct("2010-03-01 17:00:00",tz="UTC"), 
-                      to = as.POSIXct("2010-06-01 00:00:00",tz="UTC"), by="hour")
 
 
 # TESTING -----------------------------------------------------------------
@@ -227,4 +271,5 @@ buoy1.alltimes <- seq(from=as.POSIXct("2010-03-01 17:00:00",tz="UTC"),
 
 
 
-#
+
+
