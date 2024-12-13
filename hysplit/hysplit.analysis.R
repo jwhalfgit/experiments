e9650dc30c3a432a4426a6dbffb87f0d385070cb
=jwhalfgit <=john.halfacre@york.ac.uk>
2024-12-13 10:58:04 +0000

    moved the splitr plot file to the end because it's erroneous, and edited to show the seasonal.plots function is a wip

--- hysplit/hysplit.analysis.R
+++ hysplit/hysplit.analysis.R
@@ -9,6 +9,8 @@ TRAJDIR <- file.path(wd.hysplit,"trajectories","bin")
 
 library(tidyverse)
 
+# this is an edited version of trajectory_read from the splitr package.
+# It correctly adds receptor column information
 trajectory_read_alt <- function (output_folder){
   trajectory_file_list <- list.files(path = output_folder, 
                                      pattern = "^tback-.*")
@@ -85,71 +87,7 @@ trajectory_read_alt <- function (output_folder){
 
 
 
-# I have had to edit the original trajectory_plot_alt from splitr because the 
-# trajecotires were not plotting.  This is now trajectory_plot_alt.
-# It does work, but there's an issue with wrapping around the globe, so the plot 
-# looks a bit funny.  Better to use openair functions from here I think
-trajectory_plot_alt <- function (x, show_hourly = TRUE, color_scheme = "cycle_hues") 
-{
-  if (inherits(x, "trajectory_model")) {
-    if (!is.null(x$traj_df)) {
-      traj_df <- x$traj_df
-    }
-    else {
-      stop("There is no data available for plotting.")
-    }
-  }
-  if (inherits(x, "data.frame")) {
-    if (all(c("run", "receptor", "hour_along", "traj_dt", 
-              "lat", "lon", "height", "traj_dt_i") %in% colnames(x))) {
-      traj_df <- x
-    } else {
-      stop("This tibble does not contain plottable trajectory data.")
-    }
-  }
-  dt_runs <- traj_df$traj_dt_i %>% unique() %>% length()
-  if (color_scheme == "cycle_hues") {
-    colors <- (scales::hue_pal(c = 90, l = 70))(dt_runs)
-  }else if(color_scheme == "increasingly_gray") {
-    colors <- (scales::grey_pal(0.7, 0.1))(dt_runs)
-  }
-  traj_df$lon[which(traj_df$lon > 0)] <- traj_df$lon[which(traj_df$lon > 
-                                                             0)] - (180 * 2)
-  receptors <- traj_df %>% dplyr::pull(receptor) %>% unique()
-  dates <- traj_df %>% dplyr::pull(traj_dt_i) %>% unique()
-  traj_plot <- leaflet::leaflet() %>% 
-    leaflet::addProviderTiles(provider = "OpenStreetMap", group = "OpenStreetMap") %>% 
-    leaflet::addProviderTiles(provider = "CartoDB.DarkMatter",group = "CartoDB Dark Matter") %>% 
-    leaflet::addProviderTiles(provider = "CartoDB.Positron",group = "CartoDB Positron") %>% 
-    leaflet::addProviderTiles(provider = "Esri.WorldTerrain", group = "ESRI World Terrain") %>% 
-    #leaflet::addProviderTiles(provider = "Stamen.Toner", group = "Stamen Toner") %>% 
-    leaflet::fitBounds(lng1 = min(traj_df[["lon"]]), lat1 = min(traj_df[["lat"]]), lng2 = max(traj_df[["lon"]]), lat2 = max(traj_df[["lat"]])) %>% 
-    leaflet::addLayersControl(baseGroups = c("CartoDB Positron", "CartoDB Dark Matter", "Stamen Toner", "ESRI World Terrain"), overlayGroups = c("trajectory_points", "trajectory_paths"), position = "topright")
 
-  
-  for (i in seq_along(receptors)) {
-    receptor_i <- receptors[i]
-    for (j in seq_along(dates)) {
-      date_i <- dates[j]
-      wind_traj_ij <- traj_df %>% dplyr::filter(receptor == 
-                                                  receptor_i, traj_dt_i == date_i)
-      popup <- paste0("<strong>trajectory</strong> ", wind_traj_ij[["traj_dt_i"]], 
-                      "<br><strong>at time</strong> ", wind_traj_ij[["traj_dt"]], 
-                      " (", wind_traj_ij[["hour_along"]], " h)<br><strong>height</strong> ", 
-                      wind_traj_ij[["height"]], " <font size=\"1\">m AGL</font> / ", 
-                      "<strong>P</strong> ", wind_traj_ij[["pressure"]], 
-                      " <font size=\"1\">hPa</font>")
-      traj_plot <- traj_plot %>% leaflet::addPolylines(lng = wind_traj_ij[["lon"]], 
-                                                       lat = wind_traj_ij[["lat"]], group = "trajectory_paths", 
-                                                       weight = 2, smoothFactor = 1, color = colors[j]) %>% 
-        leaflet::addCircles(lng = wind_traj_ij[["lon"]], 
-                            lat = wind_traj_ij[["lat"]], group = "trajectory_points", 
-                            radius = 250, fill = TRUE, color = colors[j], 
-                            fillColor = colors[j], popup = popup)
-    }
-  }
-  traj_plot
-}
 # as a reminder, here are the sites that are referenced in trajDF:
 # 
 # sites
@@ -190,18 +128,88 @@ bc <- trajDF_oa %>% filter(receptor == 5)
 cc <- trajDF_oa %>% filter(receptor == 6)
 
 
-# 
+# WIP!!!!
 seasonal.plots <- function(DF){
   
   dfFiltered <- selectByDate(trajDF_oa, start = "01/1/2023",
                end = "31/3/2023" )
  
    
-
-  
  
-}
 
+  }
+
+
+
+
+# I have had to edit the original trajectory_plot_alt from splitr because the 
+# trajecotires were not plotting.  This is now trajectory_plot_alt.
+# It does work, but there's an issue with wrapping around the globe, so the plot 
+# looks a bit funny.  Better to use openair functions from here I think.
+
+
+
+# trajectory_plot_alt <- function (x, show_hourly = TRUE, color_scheme = "cycle_hues") 
+# {
+#   if (inherits(x, "trajectory_model")) {
+#     if (!is.null(x$traj_df)) {
+#       traj_df <- x$traj_df
+#     }
+#     else {
+#       stop("There is no data available for plotting.")
+#     }
+#   }
+#   if (inherits(x, "data.frame")) {
+#     if (all(c("run", "receptor", "hour_along", "traj_dt", 
+#               "lat", "lon", "height", "traj_dt_i") %in% colnames(x))) {
+#       traj_df <- x
+#     } else {
+#       stop("This tibble does not contain plottable trajectory data.")
+#     }
+#   }
+#   dt_runs <- traj_df$traj_dt_i %>% unique() %>% length()
+#   if (color_scheme == "cycle_hues") {
+#     colors <- (scales::hue_pal(c = 90, l = 70))(dt_runs)
+#   }else if(color_scheme == "increasingly_gray") {
+#     colors <- (scales::grey_pal(0.7, 0.1))(dt_runs)
+#   }
+#   traj_df$lon[which(traj_df$lon > 0)] <- traj_df$lon[which(traj_df$lon > 
+#                                                              0)] - (180 * 2)
+#   receptors <- traj_df %>% dplyr::pull(receptor) %>% unique()
+#   dates <- traj_df %>% dplyr::pull(traj_dt_i) %>% unique()
+#   traj_plot <- leaflet::leaflet() %>% 
+#     leaflet::addProviderTiles(provider = "OpenStreetMap", group = "OpenStreetMap") %>% 
+#     leaflet::addProviderTiles(provider = "CartoDB.DarkMatter",group = "CartoDB Dark Matter") %>% 
+#     leaflet::addProviderTiles(provider = "CartoDB.Positron",group = "CartoDB Positron") %>% 
+#     leaflet::addProviderTiles(provider = "Esri.WorldTerrain", group = "ESRI World Terrain") %>% 
+#     #leaflet::addProviderTiles(provider = "Stamen.Toner", group = "Stamen Toner") %>% 
+#     leaflet::fitBounds(lng1 = min(traj_df[["lon"]]), lat1 = min(traj_df[["lat"]]), lng2 = max(traj_df[["lon"]]), lat2 = max(traj_df[["lat"]])) %>% 
+#     leaflet::addLayersControl(baseGroups = c("CartoDB Positron", "CartoDB Dark Matter", "Stamen Toner", "ESRI World Terrain"), overlayGroups = c("trajectory_points", "trajectory_paths"), position = "topright")
+# 
+#   
+#   for (i in seq_along(receptors)) {
+#     receptor_i <- receptors[i]
+#     for (j in seq_along(dates)) {
+#       date_i <- dates[j]
+#       wind_traj_ij <- traj_df %>% dplyr::filter(receptor == 
+#                                                   receptor_i, traj_dt_i == date_i)
+#       popup <- paste0("<strong>trajectory</strong> ", wind_traj_ij[["traj_dt_i"]], 
+#                       "<br><strong>at time</strong> ", wind_traj_ij[["traj_dt"]], 
+#                       " (", wind_traj_ij[["hour_along"]], " h)<br><strong>height</strong> ", 
+#                       wind_traj_ij[["height"]], " <font size=\"1\">m AGL</font> / ", 
+#                       "<strong>P</strong> ", wind_traj_ij[["pressure"]], 
+#                       " <font size=\"1\">hPa</font>")
+#       traj_plot <- traj_plot %>% leaflet::addPolylines(lng = wind_traj_ij[["lon"]], 
+#                                                        lat = wind_traj_ij[["lat"]], group = "trajectory_paths", 
+#                                                        weight = 2, smoothFactor = 1, color = colors[j]) %>% 
+#         leaflet::addCircles(lng = wind_traj_ij[["lon"]], 
+#                             lat = wind_traj_ij[["lat"]], group = "trajectory_points", 
+#                             radius = 250, fill = TRUE, color = colors[j], 
+#                             fillColor = colors[j], popup = popup)
+#     }
+#   }
+#   traj_plot
+# }
 
 
 
