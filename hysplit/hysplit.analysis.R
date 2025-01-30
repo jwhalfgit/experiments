8eb3714cb1833c12c84d8bf604c258bf3dcfd082
John Halfacre <john.halfacre@york.ac.uk>
2025-01-30 09:29:53 +0000

    edited trjaectory_read_alt() to load various years of trajectories. added code to calculate differences between trajectories based on receptor sites

--- hysplit/hysplit.analysis.R
+++ hysplit/hysplit.analysis.R
@@ -1,19 +1,45 @@
 library(splitr)
 library(openair)
 library(gridExtra)
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(ggallin)
+
 
 met.file.loc <- "C:/HYSPLIT/metfiles/" # HYSPLIT met file directory
 wd.hysplit <- "C:/HYSPLIT/working" # HYSPLIT working directory
-TRAJDIR <- file.path(wd.hysplit,"trajectories","bin")
+TRAJDIR <- "G:/My Drive/Experiments/DEFRA/hysplit/trajectories/"
+PLOTDIR <- file.path(TRAJDIR,"plots/")
 
 
-library(tidyverse)
+sites <- as.data.frame(matrix(data = c(53.44,-2.21,"Manchester Air Quality Site",
+                                       52.45, -1.93, "Birmingham Super Site",
+                                       54.98, -1.61, "Newcastle Center",
+                                       55.95, -3.20, "Edinburgh Center",
+                                       54.60, -5.93, "Belfast Center",
+                                       51.48, -3.18, "Cardiff Center",
+                                       51.45, -0.04,  "London Super Site"),
+                              ncol=3,byrow=TRUE,dimnames = list(c(),c("lat","lon","loc"))))
+sites$lat <- as.numeric(sites$lat)
+sites$lon <- as.numeric(sites$lon)
 
 # this is an edited version of trajectory_read from the splitr package.
-# It correctly adds receptor column information
-trajectory_read_alt <- function (output_folder){
-  trajectory_file_list <- list.files(path = output_folder, 
-                                     pattern = "^tback-.*")
+# It correctly adds receptor column information.  It is important
+# the HYSPLIT output file have the prefix "tback"
+trajectory_read_alt <- function (output_folder, YEARS){
+  if(is.na(YEARS)!=TRUE){
+    trajectory_file_list <- list.files(path = output_folder, 
+                                       pattern = "^tback-.*")
+  }else{
+    tfl <- list()
+    
+    for(ix in 1:length(YEARS)){
+      tfl[[ix]]<- list.files(path = output_folder, 
+                             pattern = paste0("tback-", YEARS[ix]))
+    }
+    trajectory_file_list <- unlist(tfl)
+  }
   traj_tbl <- dplyr::tibble(receptor = integer(0), year = integer(0), 
                             month = integer(0), day = integer(0), hour = integer(0), 
                             hour_along = integer(0), lat = numeric(0), lon = numeric(0), 
@@ -92,13 +118,13 @@ trajectory_read_alt <- function (output_folder){
 # 
 # sites
 # lat   lon                         loc
-# 1 53.44 -2.21 Manchester Air Quality Site
-# 2 52.45 -1.93       Birmingham Super Site
-# 3 54.98 -1.61            Newcastle Center
-# 4 55.95 -3.20            Edinburgh Center
-# 5 54.60 -5.93              Belfast Center
-# 6 51.48 -3.18              Cardiff Center
-# 7 51.45 -0.04 London Super Site
+# 1 53.44 -2.21  Manchester Air Quality Site
+# 2 52.45 -1.93  Birmingham Super Site
+# 3 54.98 -1.61  Newcastle Center
+# 4 55.95 -3.20  Edinburgh Center
+# 5 54.60 -5.93  Belfast Center
+# 6 51.48 -3.18  Cardiff Center
+# 7 51.45 -0.04  London Super Site
 
 
 trajDF <- trajectory_read_alt(TRAJDIR) # this loads the data per splitr.
@@ -115,7 +141,8 @@ trajDF_oa <- trajDF %>%
                          `3` = sites$loc[3], 
                          `4` = sites$loc[4], 
                          `5` = sites$loc[5], 
-                         `6` = sites$loc[6]))
+                         `6` = sites$loc[6],
+                         `7` = sites$loc[7],))
 
 
 
@@ -126,20 +153,203 @@ nc <- trajDF_oa %>% filter(receptor == 3)
 ec <- trajDF_oa %>% filter(receptor == 4)
 bc <- trajDF_oa %>% filter(receptor == 5)
 cc <- trajDF_oa %>% filter(receptor == 6)
+lc <- trajDF_oa %>% filter(receptor == 7)
 
 
-# WIP!!!!
-seasonal.plots <- function(DF){
+# 
+# dfJFM <- selectByDate(trajDF_oa, start = "01/01/2023",
+#               end = "31/03/2023" )
+#  
+# png(filename = paste0(PLOTDIR,"JFM.png"), 
+#     width = 1920, height = 1080, pointsize = 32,res=200)
+# x <-trajLevel(dfJFM,type = "siteid",statistic = "frequency",plot="FALSE")
+# dev.off()
+# 
+# 
+# dfAMJ <- selectByDate(trajDF_oa, start = "01/04/2023",
+#                       end = "30/06/2023" )
+# 
+# png(filename = paste0(PLOTDIR,"AMJ.png"), 
+#     width = 1920, height = 1080, pointsize = 24)
+# trajLevel(dfAMJ,type = "siteid")
+# dev.off()
+# 
+# dfJAS <- selectByDate(trajDF_oa, start = "01/07/2023",
+#                       end = "30/09/2023" )
+# 
+# png(filename = paste0(PLOTDIR,"JAS.png"), 
+#     width = 1920, height = 1080, pointsize = 24)
+# trajLevel(dfJAS,type = "siteid")
+# dev.off()
+# 
+# dfOND <- selectByDate(trajDF_oa, start = "01/10/2023",
+#                       end = "31/12/2023" )
+# 
+# png(filename = paste0(PLOTDIR,"OND.png"), 
+#     width = 1920, height = 1080, pointsize = 24)
+# trajLevel(dfOND,type = "siteid")
+# dev.off()
+
+
+
+# Comparison of Manc and Newc with Birm -----------------------------------
+DIFFPLOTDIR <- "G:/My Drive/Experiments/DEFRA/hysplit/differences/"
+
+
+plotTrajDiff <- function(DF, FILTER, SITE1, SITE2){
+  dfFilter <- DF# %>% 
+    #filter(between(dcount,-1*FILTER,FILTER))
   
-  dfFiltered <- selectByDate(trajDF_oa, start = "01/1/2023",
-               end = "31/3/2023" )
- 
-   
- 
+  # Transform the coordinates to for plotting on a polar map
+  dfTransform <- st_as_sf(dfFilter,coords = c("xgrid","ygrid"), crs = 4326)
+  
+  # Because we're plotting frequencies, let's make grids to fill
+  grid_sf <- st_make_grid(dfTransform, cellsize = c(1, 1), square = TRUE) %>% 
+    st_sf(geometry = .) %>% 
+    st_join(dfTransform, join = st_intersects) %>% 
+    st_transform(crs = st_crs("+proj=laea +lat_0=54 +lon_0=-2"))
+    #st_transform(crs = 4326)
+  
+  #df_grid <- st_join(grid_sf, dfTransform, join = st_intersects)
+  
+  # Prep a map
+  world <- ne_countries(scale = "medium", returnclass = "sf")
+  
+  #Project the map into a Lambert projection centered around the UK
+  lambert_crs <- st_crs("+proj=laea +lat_0=54 +lon_0=-2")
+  # world_lambert <- st_transform(world, lambert_crs)
+  #df_grid_lambert <- st_transform(df_grid, lambert_crs)
+  
+  # Tell ggplot what plotting window you want, but this will create a square plot
+  bbox <- st_bbox(c(xmin = -30, xmax = 20, ymin = 30, ymax = 70), crs = st_crs(4326))
+  bbox_lambert <- st_transform(st_as_sfc(bbox), lambert_crs)
+  
+  xlim_4326 <- st_bbox(grid_sf)[c(1,3)]
+  ylim_4326 <- st_bbox(grid_sf)[c(2,4)]
+  
+  # plot origin points
+  points <- data.frame(
+    lat=c(SITE1$lat[1],SITE2$lat[1]),
+    lon=c(SITE1$lon[1],SITE2$lon[1])
+  )
 
+  site1Name <- sites$loc[which(sites$lat==points[1,1]&sites$lon == points[1,2])]
+  site2Name <- sites$loc[which(sites$lat==points[2,1]&sites$lon == points[2,2])]
+  
+  points_sf <- st_as_sf(points, coords = c("lon", "lat"), crs = 4326)
+  points_lambert <- st_transform(points_sf, lambert_crs)
+
+  # Make the plot!
+  ggplot() + 
+    geom_sf(data = grid_sf, aes(fill = dcount, color = NA),color = NA,alpha = 0.8)+
+    # #scale_fill_gradientn(colors = rainbow(7), name = "Difference") +
+    scale_fill_distiller(palette = "RdYlBu", limits = c(-1000,1000),name = "Difference", trans = pseudolog10_trans,
+                          breaks=c(-1000,-100,-10,0,10,100,1000),na.value = "transparent") +
+    theme_bw()+
+    geom_point(data = points_lambert, aes(x = st_coordinates(points_lambert)[,1], y = st_coordinates(points_lambert)[,2]), color = "blue", size = 3, shape = 21, fill = "yellow")+
+    geom_sf(data = world, fill =NA, color = "black")+
+    coord_sf(xlim = st_bbox(bbox_lambert)[c("xmin", "xmax")], ylim = st_bbox(bbox_lambert)[c("ymin", "ymax")], expand = TRUE) +
+    #coord_sf(crs = st_crs(3035), xlim = c(500000, 4500000), ylim = c(2000000, 7000000),expand=TRUE)+
+    # #coord_sf(crs = st_crs(3035), xlim = c(500000, 4500000), ylim = c(2000000, 7000000),expand=TRUE)+
+    labs(title = paste(site1Name,"vs", site2Name), x = "Longitude", y = "Latitude")
+}
+
+
+calcDiff <- function(SITE1,SITE2 = bss, 
+                     DATE1 = "01/01/2023", DATE2 = "31/03/2023",
+                     PLOT = TRUE, FILTER = 500){
+  
+  df1 <- selectByDate(SITE1, start = DATE1,
+                      end = DATE2)
+  x <-trajLevel(df1,statistic = "frequency",plot="FALSE") %>% 
+    `[[`("data") %>% 
+    select(xgrid,ygrid,count) %>% 
+    rename(df1_count = count) 
+
+  df2 <- selectByDate(SITE2, start = DATE1,
+                      end = DATE2)
+  y <-trajLevel(df2,statistic = "frequency",plot="FALSE") %>% 
+    `[[`("data") %>% 
+    select(xgrid,ygrid,count) %>% 
+    rename(df2_count = count)
+
+  
+  dfJoin <- x %>% 
+    full_join(y) %>% 
+    replace_na(list(df1_count = 0,df2_count = 0)) %>% 
+    filter(df1_count > 5 | df2_count > 5) %>% 
+    mutate(dcount = df2_count - df1_count)
+  
+  if(PLOT == TRUE){
+    outPlot <- plotTrajDiff(dfJoin,FILTER,SITE1,SITE2)
+    print(outPlot)
+    ggsave(paste0(DIFFPLOTDIR,gsub("-","",dmy(DATE1)),gsub("-","",dmy(DATE2)),
+                  "-",deparse(substitute(SITE1)),deparse(substitute(SITE2)),
+                  ".png"), width = 12.80, height = 7.68, units = "in")
   }
+  
+
+  
+  return(dfJoin)
+}
 
 
+seasDates <- data.frame(START = c("01/01/2023","01/04/2023","01/07/2023", "01/10/2023"),
+                        STOP = c("31/03/2023","30/06/2023","30/09/2023", "31/12/2023"))
+DATE1 <- "01/04/2023"
+DATE2 <- "30/06/2023"
+
+
+maqsBss <- list()
+for(ix in 1:nrow(seasDates)){
+  maqsBss[[ix]] <- calcDiff(SITE1 = maqs, SITE2 = bss,
+                            DATE1 = seasDates[ix,1],DATE2 = seasDates[ix,2], 
+                            PLOT = TRUE)
+}
+
+
+ncBss <- list()
+for(ix in 1:nrow(seasDates)){
+  ncBss[[ix]] <- calcDiff(SITE1 = nc, SITE2 = bss,
+                            DATE1 = seasDates[ix,1],DATE2 = seasDates[ix,2], 
+                            PLOT = TRUE)
+}
+# Testing code for taking differences -------------------------------------
+# 
+# maqsJFM <- selectByDate(maqs, start = DATE1,end = DATE2 )
+# x <-trajLevel(maqsJFM,statistic = "frequency",plot="FALSE") %>% 
+#   `[[`("data") %>% 
+#   select(xgrid,ygrid,count) %>% 
+#   rename(maqs_count = count)
+# 
+# bssJFM <- selectByDate(bss, start = DATE1,end = DATE2 )
+# y <-trajLevel(bssJFM,statistic = "frequency",plot=FALSE)%>% 
+#   `[[`("data") %>% 
+#   select(xgrid,ygrid,count) %>% 
+#   rename(bss_count = count)
+# 
+# ncJFM <- selectByDate(nc, start = DATE1,end = DATE2 )
+# z <-trajLevel(ncJFM,statistic = "frequency",plot=FALSE)%>% 
+#   `[[`("data") %>% 
+#   select(xgrid,ygrid,count) %>% 
+#   rename(nc_count = count)
+# 
+# 
+# 
+# maqsBss <- x %>% 
+#   full_join(y) %>% 
+#   replace_na(list(maqs_count = 0,bss_count = 0)) %>% 
+#   mutate(dcount = bss_count - maqs_count)
+# 
+# ncBss <-  z %>% 
+#   full_join(y) %>% 
+#   replace_na(list(bss_count = 0, nc_count = 0)) %>% 
+#   mutate(dcount = bss_count - nc_count)
+# 
+
+
+
+# Old ---------------------------------------------------------------------
 
 
 # I have had to edit the original trajectory_plot_alt from splitr because the 
