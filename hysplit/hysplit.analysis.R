b361e5c9b41dd0c93d16e605640d3bff81c8d7b4
John Halfacre <john.halfacre@york.ac.uk>
2025-02-05 16:37:00 +0000

    Added difference plot options

--- hysplit/hysplit.analysis.R
+++ hysplit/hysplit.analysis.R
@@ -5,6 +5,12 @@ library(tidyverse)
 library(sf)
 library(rnaturalearth)
 library(ggallin)
+library(parallel)
+library(foreach)
+
+library(foreach)
+library(doParallel)
+registerDoParallel(cores= 4)
 
 
 met.file.loc <- "C:/HYSPLIT/metfiles/" # HYSPLIT met file directory
@@ -12,6 +18,7 @@ wd.hysplit <- "C:/HYSPLIT/working" # HYSPLIT working directory
 TRAJDIR <- "G:/My Drive/Experiments/DEFRA/hysplit/trajectories/"
 PLOTDIR <- file.path(TRAJDIR,"plots/")
 
+YEAR = 2021
 
 sites <- as.data.frame(matrix(data = c(53.44,-2.21,"Manchester Air Quality Site",
                                        52.45, -1.93, "Birmingham Super Site",
@@ -19,7 +26,8 @@ sites <- as.data.frame(matrix(data = c(53.44,-2.21,"Manchester Air Quality Site"
                                        55.95, -3.20, "Edinburgh Center",
                                        54.60, -5.93, "Belfast Center",
                                        51.48, -3.18, "Cardiff Center",
-                                       51.45, -0.04,  "London Super Site"),
+                                       51.45, -0.04,  "London Super Site",
+                                       51.08, -4.10, "Barnstaple Center"),
                               ncol=3,byrow=TRUE,dimnames = list(c(),c("lat","lon","loc"))))
 sites$lat <- as.numeric(sites$lat)
 sites$lon <- as.numeric(sites$lon)
@@ -52,7 +60,100 @@ trajectory_read_alt <- function (output_folder, YEARS){
   standard_col_names <- c("receptor","year", "month", "day", "hour", "hour_along", 
                           "lat", "lon", "height", "pressure")
   
-  for (file_i in trajectory_file_list) {
+  # have replaced the default for loop with a parallel for loop.  Hopefully quickens the loading for
+  # these 72 hour back trajectories.
+  for (file_i in trajectory_file_list){
+                        file_i_path <- file.path(output_folder, file_i)
+                        file_lines <- readLines(file_i_path, encoding = "UTF-8", 
+                                                skipNul = TRUE)
+                        file_one_line <- readr::read_file(file_i_path)
+                        header_line <- file_lines %>% vapply(FUN.VALUE = logical(1), 
+                                                             USE.NAMES = FALSE, function(x) splitr:::tidy_grepl(x, "PRESSURE")) %>% 
+                          which()
+                        file_lines_data <- file_lines[(header_line + 1):(length(file_lines))] %>% 
+                          splitr:::tidy_gsub("\\s\\s*", " ") %>% splitr:::tidy_gsub("^ ", "")
+                        if (!file_one_line %>% splitr:::tidy_grepl("AIR_TEMP")) {
+                          traj_tbl_i <- file_lines_data %>% strsplit("\\s+") %>% 
+                            lapply(FUN = function(x) {
+                              x[c(1,3:6, 9:13)] %>% as.numeric() %>% stats::setNames(standard_col_names) %>% 
+                                as.list() %>% dplyr::as_tibble()
+                            }) %>% dplyr::bind_rows() %>% dplyr::mutate_at(.vars = dplyr::vars(receptor,year, 
+                                                                                               month, day, hour, hour_along), .funs = as.integer) %>% 
+                            dplyr::mutate(run = which(trajectory_file_list == file_i)) %>% 
+                            dplyr::mutate(year_full = ifelse(year < 50, year + 
+                                                               2000, year + 1900)) %>% tidyr::unite(col = date_str, 
+                                                                                                    year_full, month, day, sep = "-", remove = FALSE) %>% 
+                            tidyr::unite(col = date_h_str, date_str, hour, 
+                                         sep = " ", remove = FALSE) %>% dplyr::mutate(traj_dt = lubridate::ymd_h(date_h_str)) %>% 
+                            dplyr::select(-c(date_h_str, date_str, year_full)) %>% 
+                            dplyr::mutate(traj_dt_i = traj_dt[1])
+                        
+                          traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i) 
+                        }
+                        if (file_one_line %>% splitr:::tidy_grepl("AIR_TEMP")) {
+                          file_lines_data_20 <- file_lines_data %>% vapply(FUN.VALUE = logical(1), 
+                                                                           USE.NAMES = FALSE, function(x) {
+                                                                             tidy_grepl(x, paste0("^", rep("[0-9\\.-]*?", 
+                                                                                                           20) %>% paste(collapse = " "), "$"))
+                                                                           })
+                          file_lines_data_02 <- file_lines_data %>% vapply(FUN.VALUE = logical(1), 
+                                                                           USE.NAMES = FALSE, function(x) {
+                                                                             tidy_grepl(x, paste0("^", rep("[0-9\\.-]*?", 
+                                                                                                           2) %>% paste(collapse = " "), "$"))
+                                                                           })
+                          traj_tbl_i <- paste(file_lines_data[file_lines_data_20], 
+                                              file_lines_data[file_lines_data_02]) %>% strsplit("\\s+") %>% 
+                            lapply(FUN = function(x) {
+                              x[c(3:6, 9:22)] %>% as.numeric() %>% stats::setNames(extended_col_names) %>% 
+                                as.list() %>% dplyr::as_tibble()
+                            }) %>% dplyr::bind_rows() %>% dplyr::mutate_at(.vars = dplyr::vars(year, 
+                                                                                               month, day, hour, hour_along), .funs = as.integer) %>% 
+                            dplyr::mutate(year_full = ifelse(year < 50, year + 
+                                                               2000, year + 1900)) %>% tidyr::unite(col = date_str, 
+                                                                                                    year_full, month, day, sep = "-", remove = FALSE) %>% 
+                            tidyr::unite(col = date_h_str, date_str, hour, 
+                                         sep = " ", remove = FALSE) %>% dplyr::mutate(traj_dt = lubridate::ymd_h(date_h_str)) %>% 
+                            dplyr::select(-c(date_h_str, date_str, year_full)) %>% 
+                            dplyr::mutate(traj_dt_i = traj_dt[1])
+
+                           traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i)
+                        }
+                      }
+  traj_tbl
+}
+
+
+
+trajectory_read_alt_Par <- function (output_folder, YEARS){
+  if(is.na(YEARS)!=TRUE){
+    trajectory_file_list <- list.files(path = output_folder, 
+                                       pattern = "^tback-.*")
+  }else{
+    tfl <- list()
+    
+    for(ix in 1:length(YEARS)){
+      tfl[[ix]]<- list.files(path = output_folder, 
+                             pattern = paste0("tback-", YEARS[ix]))
+    }
+    trajectory_file_list <- unlist(tfl)
+  }
+  traj_tbl <- dplyr::tibble(receptor = integer(0), year = integer(0), 
+                            month = integer(0), day = integer(0), hour = integer(0), 
+                            hour_along = integer(0), lat = numeric(0), lon = numeric(0), 
+                            height = numeric(0), pressure = numeric(0), traj_dt = lubridate::as_datetime("2015-01-01")[-1], 
+                            traj_dt_i = lubridate::as_datetime("2015-01-01")[-1])
+  extended_col_names <- c("year", "month", "day", "hour", "hour_along", 
+                          "lat", "lon", "height", "pressure", "theta", "air_temp", 
+                          "rainfall", "mixdepth", "rh", "sp_humidity", "h2o_mixrate", 
+                          "terr_msl", "sun_flux")
+  standard_col_names <- c("receptor","year", "month", "day", "hour", "hour_along", 
+                          "lat", "lon", "height", "pressure")
+  
+  # have replaced the default for loop with a parallel for loop.  Hopefully quickens the loading for
+  # these 72 hour back trajectories.
+  trajList<- foreach (file_i = trajectory_file_list, .combine = rbind, 
+           .packages = c("tidyverse","splitr")) %dopar% {
+  #for (file_i in trajectory_file_list[1:2]) {
     file_i_path <- file.path(output_folder, file_i)
     file_lines <- readLines(file_i_path, encoding = "UTF-8", 
                             skipNul = TRUE)
@@ -77,7 +178,8 @@ trajectory_read_alt <- function (output_folder, YEARS){
                      sep = " ", remove = FALSE) %>% dplyr::mutate(traj_dt = lubridate::ymd_h(date_h_str)) %>% 
         dplyr::select(-c(date_h_str, date_str, year_full)) %>% 
         dplyr::mutate(traj_dt_i = traj_dt[1])
-      traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i) 
+      return(traj_tbl_i)
+      #traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i) 
     }
     if (file_one_line %>% splitr:::tidy_grepl("AIR_TEMP")) {
       file_lines_data_20 <- file_lines_data %>% vapply(FUN.VALUE = logical(1), 
@@ -102,12 +204,14 @@ trajectory_read_alt <- function (output_folder, YEARS){
                                                                                 year_full, month, day, sep = "-", remove = FALSE) %>% 
         tidyr::unite(col = date_h_str, date_str, hour, 
                      sep = " ", remove = FALSE) %>% dplyr::mutate(traj_dt = lubridate::ymd_h(date_h_str)) %>% 
-        dplyr::select(-c(date_h_str, date_str, year_full)) %>% 
+         dplyr::select(-c(date_h_str, date_str, year_full)) %>% 
         dplyr::mutate(traj_dt_i = traj_dt[1])
-      traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i)
+      return(traj_tbl_i)
+      
+      #traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i)
     }
   }
-  traj_tbl
+  trajList
 }
 
 
@@ -127,7 +231,7 @@ trajectory_read_alt <- function (output_folder, YEARS){
 # 7 51.45 -0.04  London Super Site
 
 
-trajDF <- trajectory_read_alt(TRAJDIR) # this loads the data per splitr.
+trajDF <- trajectory_read_alt_Par(TRAJDIR, YEAR = YEAR) # this loads the data per splitr.
 # I will reformat to work with openair
 trajDF_oa <- trajDF %>% 
   rename(hour.inc = hour_along,
@@ -142,7 +246,7 @@ trajDF_oa <- trajDF %>%
                          `4` = sites$loc[4], 
                          `5` = sites$loc[5], 
                          `6` = sites$loc[6],
-                         `7` = sites$loc[7],))
+                         `7` = sites$loc[7]))
 
 
 
@@ -207,8 +311,16 @@ plotTrajDiff <- function(DF, FILTER, SITE1, SITE2){
   grid_sf <- st_make_grid(dfTransform, cellsize = c(1, 1), square = TRUE) %>% 
     st_sf(geometry = .) %>% 
     st_join(dfTransform, join = st_intersects) %>% 
-    st_transform(crs = st_crs("+proj=laea +lat_0=54 +lon_0=-2"))
+    st_transform(crs = st_crs("+proj=laea +lat_0=54 +lon_0=-2")) %>% 
+    mutate(dcountPlot = dcount)
     #st_transform(crs = 4326)
+  
+  # Need to define color scale cutoff.  When multiple years are
+  # aggregated, (2021-2023 so far..) individual grid frequencies may
+  # approach or exceed abs(10,000). , while for seasonal plots, it may
+  #' be closer to 1000.
+  grid_sf$dcountPlot[grid_sf$dcountPlot > 5000] = 5000
+  grid_sf$dcountPlot[grid_sf$dcountPlot < -5000] = -5000
   
   #df_grid <- st_join(grid_sf, dfTransform, join = st_intersects)
   
@@ -241,22 +353,24 @@ plotTrajDiff <- function(DF, FILTER, SITE1, SITE2){
 
   # Make the plot!
   ggplot() + 
-    geom_sf(data = grid_sf, aes(fill = dcount, color = NA),color = NA,alpha = 0.8)+
+    geom_sf(data = grid_sf, aes(fill = dcountPlot, color = NA),color = NA,alpha = 0.8)+
     # #scale_fill_gradientn(colors = rainbow(7), name = "Difference") +
-    scale_fill_distiller(palette = "RdYlBu", limits = c(-1000,1000),name = "Difference", trans = pseudolog10_trans,
-                          breaks=c(-1000,-100,-10,0,10,100,1000),na.value = "transparent") +
+    scale_fill_distiller(palette = "RdYlBu", limits = c(-5000,5000),name = "Difference", trans = pseudolog10_trans,
+                          breaks=c(-5000,-100,-10,0,10,100,5000),na.value = "transparent") +
     theme_bw()+
     geom_point(data = points_lambert, aes(x = st_coordinates(points_lambert)[,1], y = st_coordinates(points_lambert)[,2]), color = "blue", size = 3, shape = 21, fill = "yellow")+
     geom_sf(data = world, fill =NA, color = "black")+
     coord_sf(xlim = st_bbox(bbox_lambert)[c("xmin", "xmax")], ylim = st_bbox(bbox_lambert)[c("ymin", "ymax")], expand = TRUE) +
     #coord_sf(crs = st_crs(3035), xlim = c(500000, 4500000), ylim = c(2000000, 7000000),expand=TRUE)+
     # #coord_sf(crs = st_crs(3035), xlim = c(500000, 4500000), ylim = c(2000000, 7000000),expand=TRUE)+
-    labs(title = paste(site1Name,"vs", site2Name), x = "Longitude", y = "Latitude")
+    labs(title = paste(site1Name,"vs", site2Name), x = "Longitude", y = "Latitude")+
+    theme(axis.title = element_text(size = 24))
+  
 }
 
 
 calcDiff <- function(SITE1,SITE2 = bss, 
-                     DATE1 = "01/01/2023", DATE2 = "31/03/2023",
+                     DATE1, DATE2,
                      PLOT = TRUE, FILTER = 500){
   
   df1 <- selectByDate(SITE1, start = DATE1,
@@ -294,12 +408,12 @@ calcDiff <- function(SITE1,SITE2 = bss,
 }
 
 
-seasDates <- data.frame(START = c("01/01/2023","01/04/2023","01/07/2023", "01/10/2023"),
-                        STOP = c("31/03/2023","30/06/2023","30/09/2023", "31/12/2023"))
-DATE1 <- "01/04/2023"
-DATE2 <- "30/06/2023"
+seasDates <- data.frame(START = c("01/01/2021","01/04/2021","01/07/2021", "01/10/2021"),
+                        STOP = c("31/03/2021","30/06/2021","30/09/2021", "31/12/2021"))
+ DATE1 <- "01/01/2021"
+ DATE2 <- "31/12/2023"
 
-
+# Seasonal dates
 maqsBss <- list()
 for(ix in 1:nrow(seasDates)){
   maqsBss[[ix]] <- calcDiff(SITE1 = maqs, SITE2 = bss,
@@ -311,9 +425,35 @@ for(ix in 1:nrow(seasDates)){
 ncBss <- list()
 for(ix in 1:nrow(seasDates)){
   ncBss[[ix]] <- calcDiff(SITE1 = nc, SITE2 = bss,
-                            DATE1 = seasDates[ix,1],DATE2 = seasDates[ix,2], 
-                            PLOT = TRUE)
+                          DATE1 = seasDates[ix,1],DATE2 = seasDates[ix,2], 
+                          PLOT = TRUE)
 }
+
+# Aggregated Dates
+maqsBss <- calcDiff(SITE1 = maqs, SITE2 = bss,
+                          DATE1 = DATE1,DATE2 = DATE2, 
+                          PLOT = TRUE)
+
+
+
+ncBss <- calcDiff(SITE1 = nc, SITE2 = bss,
+                    DATE1 = DATE1,DATE2 = DATE2, 
+                    PLOT = TRUE)
+
+
+ecBss <- calcDiff(SITE1 = ec, SITE2 = bss,
+                  DATE1 = DATE1,DATE2 = DATE2, 
+                  PLOT = TRUE)
+
+ecLc <- calcDiff(SITE1 = ec, SITE2 = lc,
+                 DATE1 = DATE1,DATE2 = DATE2, 
+                 PLOT = TRUE)
+
+bssLc <-  calcDiff(SITE1 = bss, SITE2 = lc,
+                   DATE1 = DATE1,DATE2 = DATE2, 
+                   PLOT = TRUE)
+
+
 # Testing code for taking differences -------------------------------------
 # 
 # maqsJFM <- selectByDate(maqs, start = DATE1,end = DATE2 )
