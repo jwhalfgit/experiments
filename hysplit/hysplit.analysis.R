b786fe2a87a2311d577637d92731d4147d06b285
John Halfacre <john.halfacre@york.ac.uk>
2025-04-08 11:18:17 +0100

    added default time for sourceBoxes

--- hysplit/hysplit.analysis.R
+++ hysplit/hysplit.analysis.R
@@ -223,8 +223,8 @@ ggsave(file = paste0("G:/My Drive/Experiments/DEFRA/hysplit/sourceregions/",
 
 
 # Source Attribution Analysis - TIME SERIES
-DATE1 <- "01/01/2024"
-DATE2 <- "31/03/2024"
+DATE1 <- "01/01/2017"
+DATE2 <- "31/05/2017"
 HOURS <- 0:72
 
 barncSource <- sourceBoxes(barnc, DATE1,DATE2,HOURS, OUTPUT = "TS")
@@ -238,4 +238,156 @@ ncSource <- sourceBoxes(nc, DATE1,DATE2,HOURS, OUTPUT = "TS")
 
 grid.arrange(barncSource,bcSource,bssSource,ccSource,
              ecSource,lcSource,maqsSource,ncSource)
+
+
+
+
+lc17 <- sourceBoxes(lc, DATE1 ="01/01/2017" ,DATE2 ="31/05/2017",
+                    HOURS, OUTPUT = "TS")
+lc18 <- sourceBoxes(lc, DATE1 ="01/01/2018" ,DATE2 ="31/05/2018",
+                    HOURS, OUTPUT = "TS")
+lc19 <- sourceBoxes(lc, DATE1 ="01/01/2019" ,DATE2 ="31/05/2019",
+                    HOURS, OUTPUT = "TS")
+lc20 <- sourceBoxes(lc, DATE1 ="01/01/2020" ,DATE2 ="31/05/2020",
+                    HOURS, OUTPUT = "TS")
+lc21 <- sourceBoxes(lc, DATE1 ="01/01/2021" ,DATE2 ="31/05/2021",
+                    HOURS, OUTPUT = "TS")
+lc22 <- sourceBoxes(lc, DATE1 ="01/01/2022" ,DATE2 ="31/05/2022",
+                    HOURS, OUTPUT = "TS")
+lc23 <- sourceBoxes(lc, DATE1 ="01/01/2023" ,DATE2 ="31/05/2023",
+                    HOURS, OUTPUT = "TS")
+lc24 <- sourceBoxes(lc, DATE1 ="01/01/2024" ,DATE2 ="31/05/2024",
+                    HOURS, OUTPUT = "TS")
+
+outPlot <- arrangeGrob(lc17,lc18,lc19,lc20,lc21,lc22,lc23,lc24)
+ggsave(file = paste0("G:/My Drive/Experiments/DEFRA/hysplit/sourceregions/",
+                     gsub("-","",dmy(DATE1)),gsub("-","",dmy(DATE2)),"-",HOURS[1],"-",
+                     HOURS[length(HOURS)],"lcSourceTimeSeries.png"),plot = outPlot, 
+       width= 19.20, height = 10.8,
+       units = "in")
+
+
+
+ec17 <- sourceBoxes(ec, DATE1 ="01/01/2017" ,DATE2 ="31/05/2017",
+                    HOURS, OUTPUT = "TS")
+ec18 <- sourceBoxes(ec, DATE1 ="01/01/2018" ,DATE2 ="31/05/2018",
+                    HOURS, OUTPUT = "TS")
+ec19 <- sourceBoxes(ec, DATE1 ="01/01/2019" ,DATE2 ="31/05/2019",
+                    HOURS, OUTPUT = "TS")
+ec20 <- sourceBoxes(ec, DATE1 ="01/01/2020" ,DATE2 ="31/05/2020",
+                    HOURS, OUTPUT = "TS")
+ec21 <- sourceBoxes(ec, DATE1 ="01/01/2021" ,DATE2 ="31/05/2021",
+                    HOURS, OUTPUT = "TS")
+ec22 <- sourceBoxes(ec, DATE1 ="01/01/2022" ,DATE2 ="31/05/2022",
+                    HOURS, OUTPUT = "TS")
+ec23 <- sourceBoxes(ec, DATE1 ="01/01/2023" ,DATE2 ="31/05/2023",
+                    HOURS, OUTPUT = "TS")
+ec24 <- sourceBoxes(ec, DATE1 ="01/01/2024" ,DATE2 ="31/05/2024",
+                    HOURS, OUTPUT = "TS")
+
+outPlot <- arrangeGrob(ec17,ec18,ec19,ec20,ec21,ec22,ec23,ec24)
+ggsave(file = paste0("G:/My Drive/Experiments/DEFRA/hysplit/sourceregions/",
+                     gsub("-","",dmy(DATE1)),gsub("-","",dmy(DATE2)),"-",HOURS[1],"-",
+                     HOURS[length(HOURS)],"ecSourceTimeSeries.png"), plot = outPlot,
+       width= 19.20, height = 10.8,
+       units = "in")
+
+
+
+
+
+chilb17 <- sourceBoxes(chilb, DATE1 ="01/01/2017" ,DATE2 ="31/05/2017",
+                    HOURS, OUTPUT = "TS")
+chilb18 <- sourceBoxes(chilb, DATE1 ="01/01/2018" ,DATE2 ="31/05/2018",
+                    HOURS, OUTPUT = "TS")
+chilb19 <- sourceBoxes(chilb, DATE1 ="01/01/2019" ,DATE2 ="31/05/2019",
+                    HOURS, OUTPUT = "TS")
+chilb20 <- sourceBoxes(chilb, DATE1 ="01/01/2020" ,DATE2 ="31/05/2020",
+                    HOURS, OUTPUT = "TS")
+chilb21 <- sourceBoxes(chilb, DATE1 ="01/01/2021" ,DATE2 ="31/05/2021",
+                    HOURS, OUTPUT = "TS")
+chilb22 <- sourceBoxes(chilb, DATE1 ="01/01/2022" ,DATE2 ="31/05/2022",
+                    HOURS, OUTPUT = "TS")
+chilb23 <- sourceBoxes(chilb, DATE1 ="01/01/2023" ,DATE2 ="31/05/2023",
+                    HOURS, OUTPUT = "TS")
+chilb24 <- sourceBoxes(chilb, DATE1 ="01/01/2024" ,DATE2 ="31/05/2024",
+                    HOURS, OUTPUT = "TS")
+
+outPlot <- grid.arrange(chilb17,chilb18,chilb19,chilb20,chilb21,chilb22,chilb23,chilb24)
+
+
+###########################################################################
+# Equivalence Data --------------------------------------------------------
+###########################################################################
+barncEq <- read_csv("G:/My Drive/Experiments/DEFRA/data/barnc.csv") %>% 
+  rename(ts = `Date and Time`,
+         digitel = `Barnstaple Digitel`,
+         fidas = `Barnstaple Fidas 215`) %>% 
+  select(ts, digitel, fidas) %>% 
+  mutate(ts = dmy_hm(ts),
+         digitel = as.numeric(digitel),
+         fidas = as.numeric(fidas)) %>% 
+  drop_na() %>% 
+  mutate(delta = fidas - digitel)
+  
+bss1Eq <-  read_csv("G:/My Drive/Experiments/DEFRA/data/bss1.csv") %>% 
+  rename(ts = `Date and Time`,
+         digitel = `Birmingham A4540 Digitel`,
+         fidas = `Birmingham A4540 Fidas 215`) %>% 
+  select(ts, digitel, fidas) %>% 
+  mutate(ts = dmy_hm(ts),
+         digitel = as.numeric(digitel),
+         fidas = as.numeric(fidas)) %>% 
+  drop_na() %>% 
+  mutate(delta = fidas - digitel)
+
+
+bss2Eq <-  read_csv("G:/My Drive/Experiments/DEFRA/data/bss2.csv") %>% 
+  rename(ts = `Date and Time`,
+         psol = `Birmingham University Partisol`,
+         fidas = `Birmingham University Fidas 215`) %>% 
+  select(ts, psol, fidas) %>% 
+  mutate(ts = dmy_hm(ts),
+         psol = as.numeric(psol),
+         fidas = as.numeric(fidas)) %>% 
+  drop_na() %>% 
+  mutate(delta = fidas - psol)
+
+chilbEq <- read_csv("G:/My Drive/Experiments/DEFRA/data/chilb.csv") %>% 
+  rename(ts = `Date and Time`,
+         digitel = `Chilbolton Digitel`,
+         fidas = `Chilbolton Fidas 215`) %>% 
+  select(ts, digitel, fidas) %>% 
+  mutate(ts = dmy_hm(ts),
+         digitel = as.numeric(digitel),
+         fidas = as.numeric(fidas)) %>% 
+  drop_na() %>% 
+  mutate(delta = fidas - digitel)
+
+
+lcEq <- read_csv("G:/My Drive/Experiments/DEFRA/data/lc.csv") %>% 
+  rename(ts = `Date and Time`,
+         digitel = `Marylebone Digitel`,
+         fidas = `Marylebone Fidas 215`) %>% 
+  select(ts, digitel, fidas) %>% 
+  mutate(ts = dmy_hm(ts),
+         digitel = as.numeric(digitel),
+         fidas = as.numeric(fidas)) %>% 
+  drop_na() %>% 
+  mutate(delta = fidas - digitel)
+
+
+maqsEq <- read_csv("G:/My Drive/Experiments/DEFRA/data/maqs.csv") %>% 
+  rename(ts = `Date and Time`,
+         psol = `Manchester University Partisol`,
+         fidas = `Manchester University Fidas 215`) %>% 
+  select(ts, psol, fidas) %>% 
+  mutate(ts = dmy_hm(ts),
+         psol = as.numeric(psol),
+         fidas = as.numeric(fidas)) %>% 
+  drop_na() %>% 
+  mutate(delta = fidas - psol)
+
+
+
 
