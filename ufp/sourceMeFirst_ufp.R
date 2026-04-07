667a18ed6af40e3f7e2efa99ca32ac7abc74ef8d
John Halfacre <john.halfacre@york.ac.uk>
2026-04-07 16:12:37 +0100

    initial commit

---
+++ ufp/sourceMeFirst_ufp.R
@@ -0,0 +1,8 @@
+# Please source first to load required packages, define directory 
+# and data locations.
+
+library(tidyverse)
+
+PROJDIR <- "G:/My Drive/Experiments/DEFRA/ufp/"
+
+DATADIR <- file.path(PROJDIR, "data")
