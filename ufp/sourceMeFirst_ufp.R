563b87f05c5d093ebd292f7a4f8cf72450069fc1
John <john.halfacre@york.ac.uk>
2026-04-08 12:16:53 +0100

    altered to remove reference to chemlap78 and replace with new working machine (Computer)

--- ufp/sourceMeFirst_ufp.R
+++ ufp/sourceMeFirst_ufp.R
@@ -1,8 +1,14 @@
 # Please source first to load required packages, define directory 
 # and data locations.
 
-library(tidyverse)
+Sys.setenv(TZ='UTC')
 
-PROJDIR <- "G:/My Drive/Experiments/DEFRA/ufp/"
+MYNAME <- system("hostname", intern = TRUE)
 
-DATADIR <- file.path(PROJDIR, "data")
+ROOT <- switch(MYNAME,
+               "Computer" = "~Documents/experiments/ufp", 
+               "nas-10-240-199-223.york.ac.uk" = "/Users/amylees/Documents/PhD",
+               "UOY21Y093" = "G:/My Drive",
+               NULL) 
+
+DATADIR <- file.path(ROOT, "data")
