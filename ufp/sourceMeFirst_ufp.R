9e6c3b3e3411f35fc9b83ffcec1543aba5b63b4e
John <john.halfacre@york.ac.uk>
2026-04-09 10:33:31 +0100

    added functionality for using different computers

--- ufp/sourceMeFirst_ufp.R
+++ ufp/sourceMeFirst_ufp.R
@@ -1,12 +1,14 @@
 # Please source first to load required packages, define directory 
 # and data locations.
 
+library(tidyverse)
+
 Sys.setenv(TZ='UTC')
 
 MYNAME <- system("hostname", intern = TRUE)
 
 ROOT <- switch(MYNAME,
-               "Computer" = "~Documents/experiments/ufp", 
+               "Computer" = "~/Documents/experiments/ufp", 
                "nas-10-240-199-223.york.ac.uk" = "/Users/amylees/Documents/PhD",
                "UOY21Y093" = "G:/My Drive",
                NULL) 
