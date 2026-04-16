0f88239b3e07097196b0facc9664bb70123f598e
John <john.halfacre@york.ac.uk>
2026-04-16 13:22:40 +0100

    now sources the function files, moved analysis to ufp-analysis.R

--- ufp/sourceMeFirst_ufp.R
+++ ufp/sourceMeFirst_ufp.R
@@ -21,6 +21,6 @@ setwd(ROOT)
 # Entry point: sources all component scripts in dependency order.
 # Each file can also be sourced individually after sourceMeFirst_ufp.R.
 
-#source("load_ufp.R")   # SMPS_SCALE, read_smps_files(), site data frames
-#source("plot_ufp.R")   # plot_smps_banana(), plot_smps_conversion()
-#source("npf_ufp.R")    # find_modes_*, link_mode_tracks(), detect_npf_events()
+source("load_ufp.R")   # SMPS_SCALE, read_smps_files(), site data frames
+source("plot_ufp.R")   # plot_smps_banana(), plot_smps_conversion()
+source("npf_ufp.R")    # find_modes_*, link_mode_tracks(), detect_npf_events()
