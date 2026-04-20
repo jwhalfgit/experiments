a2a8c2f8df4c96a30d56b102695548ff7001fd20
John <john.halfacre@york.ac.uk>
2026-04-20 14:15:18 +0100

    added procedures for creating monthly averages and generating plots for CPC data

--- ufp/ufp-analysis.R
+++ ufp/ufp-analysis.R
@@ -19,11 +19,21 @@ ff_baqsSMPS <- list.files(file.path(DATADIR, "baqs", "smps"),
                           pattern = "SMPS",
                           full.names = TRUE)
 
-baqsCPC <- lapply(ff_baqsCPC, read_csv) %>%
-  bind_rows() %>% 
-  mutate(date = dmy_hm(date))
+# baqsCPC <- read_cpc_files(ff_baqsCPC) %>%
+#   mutate(date = floor_date(date, unit = "1 hour")) %>%
+#   filter(complete.cases(.)) %>% 
+#   group_by(date) %>%
+#   summarize_all(mean,na.rm = TRUE)
+# 
+# save(baqsCPC, file = "baqsCPC.Rds")
+load("baqsCPC.Rds")
 
-baqsSMPS <- read_smps_files(ff_baqsSMPS)
+
+
+# baqsSMPS <- read_smps_files(ff_baqsSMPS)
+# save(baqsSMPS, file = "baqsSMPS.Rds")
+load("baqsSMPS.Rds")
+
 
 
 # MAQS (Manchester) -------------------------------------------------------
@@ -37,29 +47,87 @@ ff_maqsSMPS <- list.files(file.path(DATADIR, "maqs", "smps"),
                           pattern = "maqs-SMPS",
                           full.names = TRUE)
 
-maqsCPC <- lapply(ff_maqsCPC, read_csv) %>%
-  bind_rows() %>% 
-  rename(date = datetime) %>% 
-  mutate(date = floor_date(date, unit = "1 hour")) %>% 
-  group_by(date) %>% 
-  summarize_all(mean,na.rm = TRUE)
+# maqsCPC <- read_cpc_files(ff_maqsCPC) %>%
+#   mutate(date = floor_date(date, unit = "1 hour")) %>%
+#   filter(complete.cases(.)) %>% 
+#   group_by(date) %>%
+#   summarize_all(mean,na.rm = TRUE)
+# save(maqsCPC, file = "maqsCPC.Rds")
+load("maqsCPC.Rds")
 
 
 
-
-maqsSMPS <- read_smps_files(ff_maqsSMPS) %>% 
-  #rename(date = datetime) %>% 
-  mutate(date = floor_date(date, unit = "1 hour")) %>% 
-  group_by(date) %>% 
-  summarize_all(mean,na.rm = TRUE)
+#
+# maqsSMPS <- read_smps_files(ff_maqsSMPS) %>%
+#   #rename(date = datetime) %>%
+#   mutate(date = floor_date(date, unit = "1 hour")) %>%
+#   group_by(date) %>%
+#   summarize_all(mean,na.rm = TRUE)
+#
+# save(maqsSMPS, file = "maqsSMPS.Rds")
+load("maqsSMPS.Rds")
 
 
 #############################################################################
 # we need to see how close together hte overlapping MAQS CPC data are:
+START = ymd("2022-09-01")
+END = ymd("2022-11-01")
 
-plot_cpc_ts(cpc_data = maqsCPC, start = "2022-09-01", end = "2022-11-01")
-maqsCPC %>% 
-  filter(between(date,ymd("2022-09-01"), ymd("2022-11-01" ))) %>% 
-  arrange(date)
 
+plot_cpc_ts(cpc_data = maqsCPC, start = START, end = END)
+
+
+maqsCPC_monthly <- maqsCPC %>% 
+  mutate(mo = floor_date(date,"month")) %>% 
+  group_by(mo) %>% 
+  summarize(date = mean(mo), 
+            concMean = mean(conc,na.rm = TRUE), 
+            concSd = sd(conc,na.rm = TRUE))
+
+
+baqsCPC_monthly <- baqsCPC %>% 
+  mutate(mo = floor_date(date,"month")) %>% 
+  group_by(mo) %>% 
+  summarize(date = mean(mo), 
+            concMean = mean(conc,na.rm = TRUE), 
+            concSd = sd(conc, na.rm = TRUE))
+
+
+cpc_monthly <- bind_rows(
+  mutate(maqsCPC_monthly, site = "Manchester"),
+  mutate(baqsCPC_monthly, site = "Birmingham")
+)
+
+cpcTs <- ggplot(cpc_monthly, aes(x = date, y = concMean, colour = site, fill = site)) +
+  geom_ribbon(aes(ymin = concMean - concSd, ymax = concMean + concSd), alpha = 0.2, colour = NA) +
+  geom_line() +
+  scale_colour_manual(values = c("Manchester" = "steelblue", "Birmingham" = "tomato")) +
+  scale_fill_manual(  values = c("Manchester" = "steelblue", "Birmingham" = "tomato")) +
+  labs(x = NULL, y = "Particle concentration (#/cm³)", colour = NULL, fill = NULL) +
+  theme_minimal() +
+  theme(text = element_text(size = 22))
+
+
+cpcBoxWhisk <- bind_rows(
+  mutate(maqsCPC, site = "Manchester"),
+  mutate(baqsCPC,  site = "Birmingham")
+) %>%
+  mutate(year_mo = as.Date(floor_date(date, "month"))) %>%
+  ggplot(aes(x = year_mo, y = conc, colour = site, fill = site,
+             group = interaction(year_mo, site))) +
+  geom_boxplot(alpha = 0.3, outlier.shape = NA,
+               position = position_dodge(width = 20), width = 15) +
+  scale_x_date(date_breaks = "6 months", date_labels = "%b %Y") +
+  scale_colour_manual(values = c("Manchester" = "steelblue", "Birmingham" = "tomato")) +
+  scale_fill_manual(  values = c("Manchester" = "steelblue", "Birmingham" = "tomato")) +
+  labs(x = NULL, y = "Particle concentration (#/cm³)", colour = NULL, fill = NULL) +
+  theme_minimal() +
+  theme(text = element_text(size = 22), axis.text.x = element_text(angle = 45, hjust = 1))+
+  ylim(c(0,40000))
+
+
+cpc_ts_plot <- ggarrange(cpcTs, cpcBoxWhisk,nrow = 2, common.legend = TRUE,
+                         legend = "right")
+ggsave( file.path(PLOTDIR, "cpc_ts_plot.png"),plot = cpc_ts_plot,
+       width = 12.8, height =10.76, units = "in", bg = "white")
 
