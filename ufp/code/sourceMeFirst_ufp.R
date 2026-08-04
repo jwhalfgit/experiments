12a7fe9727d2bd3fa888e8af61461ddb82cc7e66
John <john.halfacre@york.ac.uk>
2026-08-04 09:54:10 +0100

    added source functions for PyNSD implementation helper files

--- ufp/code/sourceMeFirst_ufp.R
+++ ufp/code/sourceMeFirst_ufp.R
@@ -5,6 +5,11 @@ library(tidyverse)
 library(readxl)
 library(ggpubr)
 library(openair)
+library(openairmaps)
+library(htmlwidgets)
+library(leaflet)
+library(base64enc)
+library(trend)     # sens.slope(), mk.test() for custom Theil-Sen plots
 
 Sys.setenv(TZ='UTC')
 
@@ -32,5 +37,7 @@ setwd(file.path(ROOT, "code"))
 
 source("load_ufp.R")   # SMPS_SCALE, read_smps_files(), site data frames
 source("plot_ufp.R")   # plot_smps_banana(), plot_smps_conversion()
-source("npf_ufp.R")    # find_modes_*, link_mode_tracks(), detect_npf_events()
+source("npf_ufp.R")    # find_modes_*, link_mode_tracks(), detect_npf_events(), npf_prescreen()
+source("npf_physics.R") # condensation_sink(), coagulation_sink(), formation_rate(), etc.
+source("npf_classify.R") # NPF logbook, npf_classify(), npf_trace_mode(), npf_refit_logbook()
 source("prep_external.R") # for preparing for PyNSD (James Brean software)
